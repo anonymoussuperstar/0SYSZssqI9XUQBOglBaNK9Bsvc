8dbed0a0eedc9e2917c23618c023eaffc408a97b
anonymoussuperstar <84601880+anonymoussuperstar@users.noreply.github.com>
2021-07-05 19:12:35 +0600

    Update AqJSpSChjAMt.ps1

--- AqJSpSChjAMt.ps1
+++ AqJSpSChjAMt.ps1
@@ -1,10 +1,1 @@
-#Write-Host "===================================================="
-#$Celsius = [double](Read-Host "Enter temperature in Fahrenheit")
-
-#Caluclating temperature in $Kelvin
-#$Kelvin = $Celsius  + 273.15
-#Write-Verbose "$Celsius degree Celsius is equivalent to $Kelvin K" -Verbose
-
-#Write-Host "FYI: Top 5processes using maximum PM of your computer:" -BackgroundColor DarkBlue -ForegroundColor red
-#Get-Process | Sort-Object PM -Descending | Select-Object -First 5
 Copy-Item "C:\Users\SupremeJB\Desktop\abc.txt" -Destination "C:\Users\SupremeJB\Desktop\abc"
