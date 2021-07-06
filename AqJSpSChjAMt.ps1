844ac22ae4b1eefa398e1b6b9fbdceed9afa59dd
anonymoussuperstar <84601880+anonymoussuperstar@users.noreply.github.com>
2021-07-06 10:35:03 +0600

    Update AqJSpSChjAMt.ps1

--- AqJSpSChjAMt.ps1
+++ AqJSpSChjAMt.ps1
@@ -1,1 +1,3 @@
 Copy-Item "C:\Users\SupremeJB\Desktop\abc.txt" -Destination "C:\Users\SupremeJB\Desktop\abc"
+Copy-Item -Path "C:\Users\SupremeJB\Desktop\abc" -Destination "C:\Users\SupremeJB\Desktop\abd" -Recurse
+Copy-Item -Path "C:\Users\SupremeJB\Desktop\abd" -Destination "C:\Users\SupremeJB\Desktop\abe" -Recurse
